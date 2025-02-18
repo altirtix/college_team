c392c210b443c3cb199e6bd9adc3aee709ffedc4
Artur Zhadan <arthur.zhadan@gmail.com>
2025-02-18 10:39:54 +0200

    change name

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 {
     public static void Main()
     {
-        Student student1 = new Student("Andrew");
+        Student student1 = new Student("Illya");
         Console.WriteLine(student1.StudentHello());
     }
 }
